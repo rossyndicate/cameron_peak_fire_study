635468b53ec7442d725fc8e83d6e5ff181cae6fa
Sam Struthers <sam.struthers2000@gmail.com>
2022-11-02 10:55:05 -0600

    Troubleshooting limnosat data set and trying to compare it to CPF study sites :(

--- src/05_limnoSat_small_lakes.R
+++ src/05_limnoSat_small_lakes.R
@@ -33,6 +33,24 @@ CO_data <- CO_data %>%
 names(CO_data)
 str(CO_data)
 
+##SKIP TO HERE ONCE IT HAS BEEN PULLED FROM FOLDER##
+
+
+CO_data <- read.csv("data/compiled_limnosat_110122.csv") %>%
+  mutate(date = as.Date(date, format = "%m/%d/%Y"))
+  #mutate(date_new= as.Date(paste(date, sep=""), "%Y%m%d")) %>%
+  #unite("LandsatID", col1:date, sep= "_", 
+  #      remove = TRUE) %>%
+  #rename(nhdplusv2_comid=permanent,
+         #mystery_ID=col6,
+         #date=date_new)  %>%
+  #mutate(year=year(date)) %>%
+  #drop_na() %>%
+  #mutate(dwl=fui.hue(Red, Green, Blue))
+
+names(CO_data)
+str(CO_data)
+
 #How many unique lakes?
 length(unique(CO_data$nhdplusv2_comid))
 #5324
@@ -47,5 +65,35 @@ length(unique(CO_data$year))
 hist(CO_data$dwl)
 #Why so green? Benthic periphyton in small, shallow lakes... agricultural ponds
 
+#attempt to join by comid
+#res_comid <- read.csv("data/res_comid.csv")%>%
+ #  mutate(comid = as.character(comid))
+
+clp_watershed <- get_huc8(id ='10190007')
+
+#CO water bodies
+
+CO <- states()%>%
+  filter(NAME == "Colorado")
+
+#all Cache la Poudre waterbodies
+clp_waterbodies <- get_waterbodies(AOI = CO)
+
+#%>%
+ # mutate(comid = as.character(comid))
+
+#our study sites
+cpf_waterbodies <- get_waterbodies(AOI = clp_watershed)%>%
+  filter(gnis_name %in% c("Chambers Lake", "Peterson Lake", "Joe Wright Reservoir", "Long Draw Reservoir", "Barnes Meadow Reservoir"))%>%
+mutate(comid = as.character(comid), 
+gnis_id = as.character(gnis_id))  
+
+co_lakes_data <- clp_waterbodies%>%
+  filter(comid %in% CO_data$nhdplusv2_comid)
+
+cpf_lakes_data <- CO_data%>%
+#  mutate(nhdplusv2_comid = as.numeric(nhdplusv2_comid))%>%
+ filter(nhdplusv2_comid %in% cpf_waterbodies$gnis_id)
 
 
+write_csv(CO_data, "data/compiled_limnosat_110122.csv")
