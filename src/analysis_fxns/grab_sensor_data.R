faa8f59abbbdbc1f26722b2bccd434f64e7f52bb
SamStruthers <sam.struthers2000@gmail.com>
2023-11-15 13:41:19 -0700

    collates sensor data grabbed from API (cbri, chd, pbd, joei)

---
+++ src/analysis_fxns/grab_sensor_data.R
@@ -0,0 +1,25 @@
+
+#Find all files in the api directory
+dir <- "data/sensor_data/api/"
+data_files <- list.files(dir, full.names = T)
+#read in data from csv
+grab_data <- function(file_path) {
+#grab a single file
+  df <- read_csv_arrow(file_path)%>%
+    #convert to MST
+    with_tz(timestamp, tzone = "MST")%>%
+    #remove vulink data
+    filter(!grepl("vulink",name, ignore.case = TRUE))%>%
+    mutate(id = as.double(id))
+  
+  return(df)
+}
+#key params for sensor data
+key_params <- filter(sensor_meta, key_param == T)%>%
+  select(param_sonde)%>%
+  pull()
+# map grab data over all data_files
+sensor_data <- map(data_files, grab_data)%>%
+  bind_rows()%>%
+  filter(parameter %in% key_params)
+
