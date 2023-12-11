31cce75f3e2b99869a098107ffa1842940174624
SamStruthers <sam.struthers2000@gmail.com>
2023-12-11 13:34:01 -0700

    plot to view how parameter change betweeen 2021/2022 and 2023

---
+++ src/analysis_fxns/sf_res_impact_plot.R
@@ -0,0 +1,39 @@
+plot_sf_impact <- function( param = "ChlA"){
+  year <-  c(2022, 2023)
+  
+           lab_w_units <-  filter(chem_units, simple %in% param)%>%pull(combined)
+  
+  sf_chem<- filter(most_recent_chem, site_code %in% SF_Res & Year %in% year & !is.na(.data[[param]]))%>%
+    select(site_code,Date,all_of(param),location, Buffer_Level, Watershed_Level, watershed, Year )%>%
+    mutate(location = case_when(
+      location == "Stream" ~ "Beaver Creek", 
+      location == "Mainstem" ~ "South Fork", 
+      TRUE ~ location), 
+      site_group = case_when(
+        watershed == "Comanche Reservoir" ~ watershed, 
+        watershed == "Hourglass Reservoir"~ watershed, 
+        TRUE ~ "Stream"
+      ), 
+      filled_status = case_when(
+        Year == 2023 ~ "Filled (2023)", 
+        Year == 2022 ~ "NOT Filled  (2022)"
+      ))
+    
+    
+  sf_chem$location <- factor(sf_chem$location, levels = c("Inflow","Reservoir", "Outflow","Beaver Creek","South Fork"))
+  
+  ggplot(sf_chem, aes(x=location,y=.data[[param]],  color = filled_status, shape = site_group)) + 
+    geom_jitter(size = 8,width = 0.2)+
+    theme_bw(base_size = 30)+
+    scale_color_manual(name = "Reservoir Status", values = c("#0072B2", "#D55E00"))+
+    labs(color = "Reservoir Status",
+         shape = "Site Grouping", 
+         y = lab_w_units)+
+    theme(axis.title.x = element_blank())
+  
+  
+}
+plot_sf_impact()
+
+ggsave("data/figs_output/2023_figs/sf_impact_chla.jpg", width = 20, height = 12, dpi = 300)
+    
